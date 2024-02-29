e691ecb4dada0bba3bc6aa51cc7217b581c86128
KKN x CHANDAN <83773820+kkn2k19@users.noreply.github.com>
2024-02-29 19:45:22 +0530

    Create operator.cpp

---
+++ operator.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    //operators
+    //1. Arithmetic operator 
+    // +,-,/, %, * // use for calculations
+
+    // assignment operator
+    // = //for assigning values to variables 
+
+    // a = a + b;
+    // a += b;
+
+    // a = a*b;
+    // a *= b;
+
+
+    // int a = 10;
+
+    // int b = 20;
+
+    // int c = a+b;
+    // cout << "Value of Addition is :"<<c<<endl;
+
+    // c=a-b;
+    // cout << "Value of Subtraction is :"<<c<<endl;
+
+    // c=a*b;
+    // cout << "Value of Multiplication is :"<<c<<endl;
+
+    // c=a/b;
+    // cout << "Value of Division is :"<<c<<endl;
+
+    // c=a%b;
+    // cout << "Value of Modulus is :"<<c<<endl;
+
+
+    //declare vs define vs initialize
+
+    // declare : int a; //compiler know its existence but there is not memory allocation
+
+    // define : declared variable get a value after some operation.
+
+    // a = fiboancci(n);
+
+    // initialize : define a declared variable at the time of declaration.
+    
+    // int a = 10;
+
+    // Relational Operators or comparator o/p boolean
+    // 1. < or >
+    // 2. <= or >=
+    // 3. ==   // 5 == 5.0 true
+    // 4. != 
+    
+    // = vs == vs ===
+
+    //Logical operators o/p boolean
+    // 1. and or && 
+        // 0 && 0 == 0
+        // 0 && 1 == 0
+        // 1 && 1 == 1
+    
+    // 2. or syntax : ||
+        // 0 or 1 : 1
+        // 1 or 1 : 1
+        // 0 or 0 : 0
+
+    // 3. not syntax  : !
+        // true => false
+        // false => true
+
+    // 4. unary operator
+        // 1. ++ // int a = 1; a++; // 1 cout << a ; ++a; // 2
+        // 2. -- // int a = 2; a--; 1// cout << a; --a; // 1
+        
+    return 0;
+}
