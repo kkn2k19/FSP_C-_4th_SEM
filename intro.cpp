ae9daa6dfb4b617611f5bf4d2afb53a0460db319
KKN x CHANDAN <83773820+kkn2k19@users.noreply.github.com>
2024-02-29 19:51:13 +0530

    Create intro.cpp

---
+++ intro.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    cout<<"Hello World!";   //console output
+    cout<<"\nchandanOP";
+    cout << "KKNTHEGAMER" << endl;
+
+    int n;
+
+    cin >> n;  //console input
+
+    cout << n; 
+
+    return 0;
+}
+
+//Tokens
+//Small unit of a program that have special purpose
+//1. Keyword : they are predefined keyword that have special purpose
+//2. Indentifier : it refers to name of function, variable, constant, class and object etc
+//3. Constants : they are also a type of variable that cannot be change
+//4. Strings : everycharacter in double quotes "" or in single quotes ' ' and they are sequential collection of characters
+//5. Operators : arthimetic, assignment, comparator or relational, bitwise etc
+//6. Variables : variables are the container that store some values and they can be dynamic in nature
+
+//Data types : types of data 
+
+//System defined or predefined
+//1. Integer : ........, -1, 0, 1, 2, 3, .......
+//2. Float : 1.2, 0.5, -1.2, .......
+//3. Char : abc, a, hdfc
+//4. boolean : true or false, 1 or 0
+
+//User Defined
+//1. Structure (struct)
+//2. Class
+//3. Union
+//4. Enumeration
