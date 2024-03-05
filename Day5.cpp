9d4427f9aab8c5d9d4b9098d7e0c4c0976dfb1fb
KKN x CHANDAN <83773820+kkn2k19@users.noreply.github.com>
2024-03-05 11:38:27 +0530

    Update Day5.cpp

--- Day5.cpp
+++ Day5.cpp
@@ -1,10 +1,10 @@
 // 05.03.2024
 
-// Run Time Polymorphism or method overriding or dynamic binding or late binding 
+// Run Time Polymorphism or method overriding or dynamic binding or late binding
 
 //  - It is achieved when the object's method is invoked at the run time instead of compile time.
 
-//  - in c++, it is done with the help of virtual function or keyword 
+//  - in c++, it is done with the help of virtual function or keyword
 
 // #include<iostream>
 // using namespace std;
@@ -29,7 +29,7 @@
 
 // class Cat : public Animal
 // {
-//     public: 
+//     public:
 //         void sound() override
 //         {
 //             cout << "Now Cat is Meow" << endl;
@@ -50,50 +50,91 @@
 //     return 0;
 // }
 
+// BANKING SYSTEM
 
+// #include<iostream>
+// using namespace std;
 
-//BANKING SYSTEM
+// class Banking
+// {
+//     public:
+//           virtual void BankingSystem()
+//           {
+//             cout << "Please Invest Money"<< endl;
+//           }
+// };
 
-#include<iostream>
+// class Customer1 : public Banking
+// {
+//     public:
+//         void BankingSystem() override
+//         {
+//             cout << "Withdraw" << endl;
+//         }
+// };
+
+// class Customer2 : public Banking
+// {
+//     public:
+//         void BankingSystem() override
+//         {
+//             cout << "Deposit" << endl;
+//         }
+// };
+
+// int main()
+// {
+//     Banking *c1 = new  Customer1();
+//     Banking *c2 = new  Customer2();
+
+//     c1->BankingSystem();
+//     c2->BankingSystem();
+
+//     delete c1;
+//     delete c2;
+
+//     return 0;
+// }
+
+// Abstraction
+//  - it involves hiding internal details of class while exposing a simplified functionality to used who are interact with.
+
+// Abstract Class :
+//    - it is a class that cannot be instantiated or initialized on its own or own its in class.
+
+//    - it declare virtual function that meant to be initailized in child class but have no implementation in base class.
+
+//    syntax : virtual void/int/float(return type) functionName = 0;
+
+#include <iostream>
 using namespace std;
 
-class Banking
+class Shape
 {
-    public:
-          virtual void BankingSystem()
-          {
-            cout << "Please Invest Money"<< endl;
-          }
+public:
+    virtual void draw() = 0; // pure virtual function
+
+    void display()
+    {
+        cout << "Area of shape" << endl;
+    }
 };
 
-class Customer1 : public Banking
+class Rectangle : public Shape
 {
     public:
-        void BankingSystem() override
+        void draw() override
         {
-            cout << "Withdraw" << endl;
-        }
-};
-
-class Customer2 : public Banking
-{
-    public: 
-        void BankingSystem() override
-        {
-            cout << "Deposit" << endl;
-        }
+            cout << "Draw a rectangle !!!" << endl;
+        } 
 };
 
 int main()
 {
-    Banking *c1 = new  Customer1();
-    Banking *c2 = new  Customer2();
+    Shape *shape1 = new Rectangle();
+    shape1->draw();
+    shape1->display();
 
-    c1->BankingSystem();
-    c2->BankingSystem();
-
-    delete c1;
-    delete c2;
-
+    delete shape1;
     return 0;
 }
