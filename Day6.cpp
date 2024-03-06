a8c3e27d98e19952c2271463d2ac21bd74858f40
KKN x CHANDAN <83773820+kkn2k19@users.noreply.github.com>
2024-03-06 20:07:11 +0530

    Update Day6.cpp

--- Day6.cpp
+++ Day6.cpp
@@ -1,4 +1,3 @@
-https://canvas.instructure.com/courses/8848501
 // STL (standard tag linrary)
 
 //  - In C++, STL provide powerful collection of classes and functions of general purpose data structures and algorithms.
@@ -172,30 +171,151 @@ https://canvas.instructure.com/courses/8848501
 //     }
 // }
 
-
 // Map
-//  - it store data or elements into key-value pair 
+//  - it store data or elements into key-value pair
 //  - each value is stored in the form of key with their corresponding value
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int main()
-{
-    map<string,int> mp;
+// int main()
+// {
+//     map<string,int> mp;
 
-    mp["Iphone"] = 1;
-    mp["Blackberry"] = 2;
-    mp["Motorola"] = 3;
+//     mp["Iphone"] = 1;
+//     mp["Blackberry"] = 2;
+//     mp["Motorola"] = 3;
 
-    map<string, int> :: iterator it = mp.begin();
+//     map<string, int> :: iterator it = mp.begin();
 
-    while (it != mp.end())
-    {
-        cout << "Key : " << it->first << " value : " << it->second << endl;
-        it++;
-    }
+//     while (it != mp.end())
+//     {
+//         cout << "Key : " << it->first << " value : " << it->second << endl;
+//         it++;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+// Set :
+//     - store elements in sorted order
+//     - each element is unique, no duplicate element.
+//     - stored in ascending or descending
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     set<int> st = {1,2,3,6,7,8,1,1,2,8};
+
+//     for(const auto &it : st)
+//     {
+//         cout << it << " ";
+//     }
+
+//     return 0;
+// }
+
+// example of Set
+
+// an integer array is given,
+// find distinct element of an array also the no. of distinct element
+
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int countDistinct(const vector<int> &arr)
+// {
+//     set<int> distinctElem;
+
+//     for (int x : arr)
+//     {
+//         distinctElem.insert(x);
+//     }
+
+//     cout << "Distinct Elements : ";
+//         for(const auto &it : distinctElem)
+//     {
+//         cout << it << " ";
+//     }
+
+//     return distinctElem.size();
+// }
+
+// int main()
+// {
+//     vector<int> arr = {1, 2, 4, 7, 8, 6, 4, 3, 2, 1, 5, 7, 4, 7, 0, 3};
+
+//     cout << countDistinct(arr) << endl;
+
+//     return 0;
+// }
+
+//STACK  -- LIFO
+
+// int main()
+// {
+// 	stack<int> stk;
+	
+// 	stk.push(10);//for push element
+// 	stk.push(20);
+// 	stk.push(30);
+// 	stk.push(10);
+// 	stk.push(30);
+	
+// 	cout << "Size of stack : "<<stk.size()<<endl;;
+	
+// 	cout <<"Top Element : " <<stk.top()<<endl;//for top element
+// 	stk.pop();
+// 	cout <<"Top Element : " <<stk.top()<<endl;
+// 	stk.pop();
+// 	cout <<"Top Element : " <<stk.top()<<endl;
+// 	stk.pop();
+// 	cout <<"Top Element : " <<stk.top()<<endl;
+// 	stk.pop();
+// 	cout <<"Top Element : " <<stk.top()<<endl;
+// 	stk.pop();
+	
+// 	if(stk.empty())
+// 	{
+// 		cout << "Stack is empty";
+// 	}
+// 	else
+// 	{
+// 		cout << "Not Empty";
+// 	}
+// }
+
+//QUEUE   -- FIFO
+
+// int main()
+// {
+// 	queue<int> que;
+	
+// 	que.push(10);//for push element
+// 	que.push(20);
+// 	que.push(30);
+// 	que.push(10);
+// 	que.push(30);
+	
+// 	cout << "Size of Queue : "<<que.size()<<endl;;
+	
+// 	cout << "First element : " << que.front() << endl; 
+// 	cout << "Last element : " << que.back() << endl; 
+
+//     que.pop();
+//     cout << "First element : " << que.front() << endl;
+	
+// 	if(que.empty())
+// 	{
+// 		cout << "Queue is empty";
+// 	}
+// 	else
+// 	{
+// 		cout << "Not Empty";
+// 	}
+// }
