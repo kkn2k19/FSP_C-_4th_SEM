0087b9c65d9f10f3ab86a2d8c0466092bb2f5bcd
KKN x CHANDAN <83773820+kkn2k19@users.noreply.github.com>
2024-03-06 12:26:39 +0530

    Update Day6.cpp

--- Day6.cpp
+++ Day6.cpp
@@ -114,25 +114,86 @@
 
 // Vector of Vector
 
-#include <iostream>
-#include <bits/stdc++.h> // for stl library
+// #include <iostream>
+// #include <bits/stdc++.h> // for stl library
+// using namespace std;
+
+// int main()
+// {
+//     vector<vector<int>> vec;
+
+//     vec.push_back({1, 2, 3});
+//     vec.push_back({4, 5, 6});
+//     vec.push_back({7, 8, 9});
+
+//     for(const auto &row : vec)
+//     {
+//         for(int x : row)
+//         {
+//             cout << x << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+// Iterators
+
+//  - used to iterate different types of container in STL.
+
+//  - they are objects that point to memory address within STL.
+
+//  - forward iteration, backward, random(direct access), bidirectional.
+
+//  - begin() : pointing to first element.
+//  - end() : pointing to last element.
+//  - advance() : iterate by specified number.
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     vector<int> vec = {10, 20, 30, 40, 50, 60};
+
+//     for(auto it = vec.begin(); it != vec.end(); it++)
+//     {
+//         cout << *it << " ";
+//     }
+
+//     cout << endl;
+
+//     for(auto it = vec.end(); it != vec.begin(); it--)
+//     {
+//         cout << *it << " ";
+//     }
+// }
+
+
+// Map
+//  - it store data or elements into key-value pair 
+//  - each value is stored in the form of key with their corresponding value
+
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    vector<vector<int>> vec;
+    map<string,int> mp;
 
-    vec.push_back({1, 2, 3});
-    vec.push_back({4, 5, 6});
-    vec.push_back({7, 8, 9});
+    mp["Iphone"] = 1;
+    mp["Blackberry"] = 2;
+    mp["Motorola"] = 3;
 
-    for(const auto &row : vec)
+    map<string, int> :: iterator it = mp.begin();
+
+    while (it != mp.end())
     {
-        for(int x : row)
-        {
-            cout << x << " ";
-        }
-        cout << endl;
+        cout << "Key : " << it->first << " value : " << it->second << endl;
+        it++;
     }
 
     return 0;
