571e793da66744a40db11d38adf695d84246203f
KKN x CHANDAN <83773820+kkn2k19@users.noreply.github.com>
2024-03-06 10:33:33 +0530

    Add files via upload

---
+++ Day6.cpp
@@ -0,0 +1,31 @@
+//STL (standard tag linrary)
+
+ - In C++, STL provide powerful collection of classes and functions of general purpose data structures and algorithms.
+
+it include 4 types of method : 
+
+ - Container : 
+*        Vector : dynamic array that grow and shrink at runtime.
+*        List : A doubly linked list
+*        Map : An associated container that store data in key-value pair.
+*        Set : It contains distinct elements or unique element 
+{1,2,3,1,2,3}={1,2,3}
+*        Stack : LIFO
+*        Queue : FIFO
+
+ - Algorithms : 
+          sorting : sort in ascending and descending order
+          T : O(logn)   S : O(1)
+
+          Searching : Linear and == Binary
+                      - upper and lower bound
+
+          max(), min(), accumulate(), find() etc
+
+ - Iterators : 
+          - Forward iterator : forward traversal
+          - Bidirection : backward and forward traversal
+          - Random access : direct access on element
+
+ - Functors : Functors are object like function.
+              for creating custom algorithms.
