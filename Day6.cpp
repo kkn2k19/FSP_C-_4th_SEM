c778e49dfcaee24eaa6cdef5eb8cd928b62bbf65
KKN x CHANDAN <83773820+kkn2k19@users.noreply.github.com>
2024-03-06 11:08:11 +0530

    Update Day6.cpp

--- Day6.cpp
+++ Day6.cpp
@@ -1,31 +1,80 @@
 //STL (standard tag linrary)
 
- - In C++, STL provide powerful collection of classes and functions of general purpose data structures and algorithms.
+//  - In C++, STL provide powerful collection of classes and functions of general purpose data structures and algorithms.
 
-it include 4 types of method : 
+// it include 4 types of method : 
 
- - Container : 
-*        Vector : dynamic array that grow and shrink at runtime.
-*        List : A doubly linked list
-*        Map : An associated container that store data in key-value pair.
-*        Set : It contains distinct elements or unique element 
-{1,2,3,1,2,3}={1,2,3}
-*        Stack : LIFO
-*        Queue : FIFO
+//  - Container : 
+// *        Vector : dynamic array that grow and shrink at runtime.
+// *        List : A doubly linked list
+// *        Map : An associated container that store data in key-value pair.
+// *        Set : It contains distinct elements or unique element 
+//                 {1,2,3,1,2,3}={1,2,3}
+// *        Stack : LIFO
+// *        Queue : FIFO
 
- - Algorithms : 
-          sorting : sort in ascending and descending order
-          T : O(logn)   S : O(1)
+//  - Algorithms : 
+//           sorting : sort in ascending and descending order
+//           T : O(logn)   S : O(1)
 
-          Searching : Linear and == Binary
-                      - upper and lower bound
+//           Searching : Linear and == Binary
+//                       - upper and lower bound
 
-          max(), min(), accumulate(), find() etc
+//           max(), min(), accumulate(), find() etc
 
- - Iterators : 
-          - Forward iterator : forward traversal
-          - Bidirection : backward and forward traversal
-          - Random access : direct access on element
+//  - Iterators : 
+//           - Forward iterator : forward traversal
+//           - Bidirection : backward and forward traversal
+//           - Random access : direct access on element
 
- - Functors : Functors are object like function.
-              for creating custom algorithms.
+//  - Functors : Functors are object like function.
+//               for creating custom algorithms.
+
+// Pair
+//  - can store two heterogeneous object in a single unit.
+//  - store only exactly two element
+
+// #include<iostream>
+// #include<bits/stdc++.h>     // for stl library
+// using namespace std;
+
+// int main()
+// {
+//     pair<int, string> student1(101, "Karan");
+//     pair<int, float> student2(1, 3.14);
+//     pair<string, float> student3("Pie Value",3.14);
+
+//     cout << student1.first << " " << student1.second << endl;
+//     cout << student2.first << " " << student2.second << endl;
+//     cout << student3.first << " " << student3.second << endl;
+
+//     return 0;
+// }
+
+// Vector : 
+//     - a sequenced container that dynamically manages elements of an array.
+//     - used for efficient traversal.
+
+#include<iostream>
+#include<bits/stdc++.h>     // for stl library
+using namespace std;
+
+int main()
+{
+    vector<int> vec;
+
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    vec.push_back(40);
+    vec.push_back(50);
+    vec.push_back(60);
+
+    //display
+    for(int x : vec)
+    {
+        cout << x << " "; 
+    }
+
+    return 0;
+}
